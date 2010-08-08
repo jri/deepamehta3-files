785553a852b463c7fc3380e3a28ff1ac833ce32e
Jörg Richter <jri@deepamehta.de>
2010-08-08 15:13:01 +0200

    1) Tries to display video files in the detail panel. 2) "MIME Type" field renamed to "Media Type". You must reset your DB.

--- src/main/resources/web/script/dm3_files.js
+++ src/main/resources/web/script/dm3_files.js
@@ -9,24 +9,30 @@ function dm3_files() {
 
 
     this.file_dropped = function(file) {
-        alert("Dropped file:\n" + inspect(file))
+        // alert("Dropped file:\n" + inspect(file))
         var properties = {
-            "de/deepamehta/core/property/Filename": file.name,
-            "de/deepamehta/core/property/Path":     file.path,
-            "de/deepamehta/core/property/MimeType": file.type,
-            "de/deepamehta/core/property/Size":     file.size
+            "de/deepamehta/core/property/Filename":  file.name,
+            "de/deepamehta/core/property/Path":      file.path,
+            "de/deepamehta/core/property/MediaType": file.type,
+            "de/deepamehta/core/property/Size":      file.size
         }
         //
         if (file.type == "text/plain") {
             var content = "<pre>" + file.content + "</pre>"
         } else if (file.type.match(/^image\//)) {
-            var content = "<img src=\"" + local_resource_uri(file.path, file.type, file.size) + "\">"
+            var content = "<img src=\"" + local_resource_uri(file.path, file.type, file.size) + "\"></img>"
         } else if (file.type == "application/pdf") {
             var content = "<embed src=\"" + local_resource_uri(file.path, file.type, file.size) +
-                "\" width=\"100%\" height=\"100%\">"
+                "\" width=\"100%\" height=\"100%\"></embed>"
         } else if (file.type.match(/^audio\//)) {
             var content = "<embed src=\"" + local_resource_uri(file.path, file.type, file.size) +
-                "\" width=\"100%\" height=\"100\">"
+                "\" width=\"95%\" height=\"80\"></embed>"
+            // var content = "<audio controls=\"\" src=\"" + local_resource_uri(file.path, file.type, file.size) +
+            // "\"></audio>"
+        } else if (file.type.match(/^video\//)) {
+            var content = "<embed src=\"" + local_resource_uri(file.path, file.type, file.size) + "\"></embed>"
+            // var content = "<video controls=\"\" src=\"" + local_resource_uri(file.path, file.type, file.size) +
+            // "\"></video>"
         } else {
             // TODO: handle by plugins
         }
@@ -35,7 +41,7 @@ function dm3_files() {
         }
         //
         var file_topic = create_topic("de/deepamehta/core/topictype/File", properties)
-        add_topic_to_canvas(file_topic)
+        add_topic_to_canvas(file_topic, "show")
     }
 
 
