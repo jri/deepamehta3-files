e2d612bee712e036c1628dc83c2a3b9b79685f49
Jörg Richter <jri@deepamehta.de>
2010-08-09 16:44:46 +0200

    Access control for the /resource REST resource: only requests from localhost are allowed. So, the HIGH SECURITY RISC as existed in initial commit is abolished. The files on your disc are no longer accessible from another machine.

--- src/main/java/de/deepamehta/plugins/files/resources/ResourceResource.java
+++ src/main/java/de/deepamehta/plugins/files/resources/ResourceResource.java
@@ -4,11 +4,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import javax.servlet.http.HttpServletRequest;
 
 import java.io.InputStream;
 import java.net.URL;
-
 import java.util.logging.Logger;
 
 
@@ -24,10 +27,19 @@ public class ResourceResource {
 
     @GET
     @Path("/{uri}")
-    public Response getResource(@PathParam("uri") String uri, @QueryParam("type") String type,
+    public Response getResource(@PathParam("uri") String uri, @Context HttpServletRequest request,
+                                                              @QueryParam("type") String type,
                                                               @QueryParam("size") long size) throws Exception {
-        logger.info("Getting resource " + uri + " (type=\"" + type + "\", size=" + size + ")");
-        InputStream in = new URL(uri).openStream();
-        return Response.ok(in, type).header("Content-Length", size).build();
+        String localAddr = request.getLocalAddr();
+        String remoteAddr = request.getRemoteAddr();
+        boolean allowed = localAddr.equals(remoteAddr);
+        logger.info("Requesting resource " + uri + " (type=\"" + type + "\", size=" + size + ")\n  local address: " +
+            localAddr + ", remote address: " + remoteAddr + " => " + (allowed ? "ALLOWED" : "FORBIDDEN"));
+        if (allowed) {
+            InputStream in = new URL(uri).openStream();
+            return Response.ok(in, type).header("Content-Length", size).build();
+        } else {
+            return Response.status(Status.FORBIDDEN).build();
+        }
     }
 }
