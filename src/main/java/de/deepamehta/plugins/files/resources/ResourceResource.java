2de855b56390dc27789cab8e5345bca6ebddc0bc
Jörg Richter <jri@deepamehta.de>
2010-08-13 16:51:17 +0200

    1) Filebrowser folders can be dropped on the canvas. For each contained file a File topic is created. 2) Bug fix for Safari: display of plain text files works. 3) Resource resource extended: listings of local directories can be requested. 4) New hook: "directory_dropped".

--- src/main/java/de/deepamehta/plugins/files/resources/ResourceResource.java
+++ src/main/java/de/deepamehta/plugins/files/resources/ResourceResource.java
@@ -1,15 +1,23 @@
 package de.deepamehta.plugins.files.resources;
 
+import de.deepamehta.core.util.JavaUtils;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.logging.Logger;
@@ -27,19 +35,62 @@ public class ResourceResource {
 
     @GET
     @Path("/{uri}")
-    public Response getResource(@PathParam("uri") String uri, @Context HttpServletRequest request,
-                                                              @QueryParam("type") String type,
-                                                              @QueryParam("size") long size) throws Exception {
+    public Response getResource(@PathParam("uri") URL uri, @Context HttpServletRequest request,
+                                                           @QueryParam("type") String type,
+                                                           @QueryParam("size") long size) throws Exception {
         String localAddr = request.getLocalAddr();
         String remoteAddr = request.getRemoteAddr();
         boolean allowed = localAddr.equals(remoteAddr);
         logger.info("Requesting resource " + uri + " (type=\"" + type + "\", size=" + size + ")\n  local address: " +
             localAddr + ", remote address: " + remoteAddr + " => " + (allowed ? "ALLOWED" : "FORBIDDEN"));
         if (allowed) {
-            InputStream in = new URL(uri).openStream();
-            return Response.ok(in, type).header("Content-Length", size).build();
+            if (uri.getProtocol().equals("file")) {
+                File file = new File(uri.getPath());
+                if (file.isDirectory()) {
+                    return directoryResource(file);
+                }                
+            }
+            return resource(uri, type, size);
         } else {
             return Response.status(Status.FORBIDDEN).build();
         }
+    }
+
+    // ------------------------------------------------------------------------------------------------- Private Methods
+
+    private Response resource(URL uri, String type, long size) throws Exception {
+        InputStream in = uri.openStream();
+        ResponseBuilder builder = Response.ok(in);
+        if (type != null) {
+            builder.type(type);
+        }
+        if (size != 0) {
+            builder.header("Content-Length", size);
+        }
+        return builder.build();
+    }
+
+    private Response directoryResource(File directory) throws Exception {
+        JSONObject dir = new JSONObject();
+        JSONArray items = new JSONArray();
+        dir.put("kind", "directory");
+        dir.put("name", directory.getName());
+        dir.put("path", directory.getPath());
+        dir.put("items", items);
+        //
+        for (File file : directory.listFiles()) {
+            JSONObject item = new JSONObject();
+            item.put("name", file.getName());
+            item.put("path", file.getPath());
+            if (file.isDirectory()) {
+                item.put("kind", "directory");
+            } else {
+                item.put("kind", "file");
+                item.put("size", file.length());
+                item.put("type", JavaUtils.getFileType(file.getName()));
+            }
+            items.put(item);
+        }
+        return Response.ok(dir, "application/json").build();
     }
 }
--- src/main/resources/web/script/dm3_files.js
+++ src/main/resources/web/script/dm3_files.js
@@ -8,28 +8,111 @@ function dm3_files() {
 
 
 
+    this.init = function() {
+        extend_rest_client()
+    }
+
+    this.process_drop = function(data_transfer) {
+        if (contains(data_transfer.types, "Files")) {
+            if (typeof netscape != "undefined") {
+                process_file_drop_firefox(data_transfer)
+            } else {
+                process_file_drop_safari(data_transfer)
+            }
+        } else if (contains(data_transfer.types, "text/plain")) {
+            alert("WARNING: Dropped item is not processed.\n\nType: text/plain (not yet implemented)\n\n" +
+                "Text: " + data_transfer.getData("text/plain"))
+        } else {
+            alert("WARNING: Dropped item is not processed.\n\nUnexpected type (not yet implemented)\n\n" +
+                inspect(data_transfer))
+        }
+        return false;
+
+        function process_file_drop_firefox(data_transfer) {
+            try {
+                // Firefox note: a DOM File's "mozFullPath" attribute contains the file's path.
+                // Requires the UniversalFileRead privilege to read.
+                netscape.security.PrivilegeManager.enablePrivilege("UniversalFileRead")
+                for (var i = 0, file; file = data_transfer.files[i]; i++) {
+                    if (is_directory(file)) {
+                        var dropped_dir = dmc.get_resource("file:" + file.mozFullPath)
+                        trigger_hook("directory_dropped", dropped_dir)
+                        continue
+                    }
+                    var dropped_file = new File(file.name, file.mozFullPath, file.type, file.size)
+                    if (file.type == "text/plain") {
+                        read_text_file(dropped_file)
+                    }
+                    trigger_hook("file_dropped", dropped_file)
+                }
+            } catch (e) {
+                alert("Local file \"" + file.name + "\" can't be accessed.\n\n" + e)
+            }
+
+            function is_directory(file) {
+                // Firefox note 1: The path of a File object which represents a directory doesn't end with "/".
+                // So we can't detect directories safely.
+                // Firefox note 2: The size of a File object which represents a directory is 0.
+                // We exploit this fact to detect a directory heuristically.
+                // Obviously a wrong result is returned by zero-byte files.
+                return file.size == 0
+            }
+        }
+
+        function process_file_drop_safari(data_transfer) {
+            // Note: Safari provides a "text/uri-list" data flavor which holds the URIs of the files dropped
+            var uri_list = data_transfer.getData("text/uri-list").split("\n")
+            for (var i = 0, file; file = data_transfer.files[i]; i++) {
+                var path = uri_to_path(uri_list[i])
+                if (is_directory(path)) {
+                    var dropped_dir = dmc.get_resource("file:" + path)
+                    trigger_hook("directory_dropped", dropped_dir)
+                    continue
+                }
+                var dropped_file = new File(file.name, path, file.type, file.size)
+                if (file.type == "text/plain") {
+                    read_text_file(dropped_file)
+                }
+                trigger_hook("file_dropped", dropped_file)
+            }
+
+            function uri_to_path(uri) {
+                // Note: local file URIs provided by Safari begin with "file://localhost" which must be cut off
+                if (uri.match(/^file:\/\/localhost(.*)/)) {
+                    uri = RegExp.$1
+                }
+                // Note: local file URIs provided by Safari are encoded
+                return decodeURIComponent(uri)
+            }
+
+            function is_directory(path) {
+                // Safari note: The URI of a File object which represents a directory end with "/".
+                return path.match(/.$/)[0] == "/"
+            }
+        }
+    }
+
     this.file_dropped = function(file) {
-        // alert("Dropped file:\n" + inspect(file))
         var properties = {
             "de/deepamehta/core/property/Filename":  file.name,
             "de/deepamehta/core/property/Path":      file.path,
             "de/deepamehta/core/property/MediaType": file.type,
             "de/deepamehta/core/property/Size":      file.size
         }
-        //
+        // Note: for unknown file types file.type is undefined
         if (file.type == "text/plain") {
             var content = "<pre>" + file.content + "</pre>"
-        } else if (file.type.match(/^image\//)) {
+        } else if (file.type && file.type.match(/^image\//)) {
             var content = "<img src=\"" + local_resource_uri(file.path, file.type, file.size) + "\"></img>"
         } else if (file.type == "application/pdf") {
             var content = "<embed src=\"" + local_resource_uri(file.path, file.type, file.size) +
                 "\" width=\"100%\" height=\"100%\"></embed>"
-        } else if (file.type.match(/^audio\//)) {
+        } else if (file.type && file.type.match(/^audio\//)) {
             var content = "<embed src=\"" + local_resource_uri(file.path, file.type, file.size) +
                 "\" width=\"95%\" height=\"80\"></embed>"
             // var content = "<audio controls=\"\" src=\"" + local_resource_uri(file.path, file.type, file.size) +
             // "\"></audio>"
-        } else if (file.type.match(/^video\//)) {
+        } else if (file.type && file.type.match(/^video\//)) {
             var content = "<embed src=\"" + local_resource_uri(file.path, file.type, file.size) + "\"></embed>"
             // var content = "<video controls=\"\" src=\"" + local_resource_uri(file.path, file.type, file.size) +
             // "\"></video>"
@@ -44,6 +127,17 @@ function dm3_files() {
         add_topic_to_canvas(file_topic, "show")
     }
 
+    this.directory_dropped = function(dir) {
+        for (var i = 0, item; item = dir.items[i]; i++) {
+            if (item.kind == "file") {
+                if (item.type == "text/plain") {
+                    read_text_file(item)
+                }
+                trigger_hook("file_dropped", item)
+            }
+        }
+    }
+
 
 
     /************************************************************************************************/
@@ -52,7 +146,41 @@ function dm3_files() {
 
 
 
+    function extend_rest_client() {
+        /**
+         * @param   uri     Must not be URI-encoded!
+         */
+        dmc.get_resource = function(uri, type, size) {
+            var params = this.createRequestParameter({type: type, size: size})
+            return this.request("GET", "/resource/" + encodeURIComponent(uri) + params.to_query_string())
+        }
+    }
+
     function local_resource_uri(path, type, size) {
         return "/resource/file:" + encodeURIComponent(path) + "?type=" + type + "&size=" + size
+    }
+
+    // ---
+
+    function read_text_file(file) {
+        file.content = dmc.get_resource("file:" + file.path)
+    }
+
+    /*** Custom Classes ***/
+
+    function File(name, path, type, size, content) {
+        this.kind = "file"
+        this.name = name
+        this.path = path
+        this.type = type
+        this.size = size
+        this.content = content
+    }
+
+    function Directory(name, path, items) {
+        this.kind = "directory"
+        this.name = name
+        this.path = path
+        this.items = items  // array of File and Directory objects
     }
 }
